e4d4b05c283dc27d9c3b76b122b13135aacf4a63
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-22 15:48:27 -0300

    Nuevo componente: UserTable
    - Se ha creado un nuevo componente UserTable, el cual retorna una tabla con todos los usuarios creados.

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,7 +1,18 @@
-
-import React from 'react';
+import React, { useState } from "react";
+import UserTable from "./components/UserTable";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
+
+  //state
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,6 +22,7 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
